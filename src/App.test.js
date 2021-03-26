c180b22b3cebe0b495ed6912fbbba034c5975f10
DC <jessy.charlot@outlook.fr>
2021-03-26 20:12:44 +0100

    remove test

--- src/App.test.js
+++ src/App.test.js
@@ -8,12 +8,6 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 test('renders new component', () => {
   const messageToTest = `Hello, i'm Jessy`
   render(<NewComponent>{messageToTest}</NewComponent>);
